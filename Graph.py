11d252750413ade3d702e5ae19747b202b956433
SHIO777 <shiotatsu1015@gmail.com>
2020-10-10 21:19:07 +0900

    1010 21:20

---
+++ Graph.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import subprocess as proc
+
+
+path = "/home/pi/Desktop/TeamB/" 
+graphfile1 = "graph1.png"
+graphfile2 = "graph2.png"
+filename21 = "mini_thro.txt" #最小値
+filename22 = "maxi_thro.txt" #最大値
+filename23 = "x_thro.txt" #傾き変わる点(x)
+filename24 = "y_thro.txt" #傾き変わる点(y)
+
+
+
+def makeGraph():
+    range12=[]
+    x = []
+    f = open(path+filename21, "r")
+    mini21 = int(f.read())
+    f.close()
+    f = open(path+filename22, "r")
+    maxi21 = int(f.read())
+    f.close()
+    f = open(path+filename23, "r")
+    x21 = int(f.read())
+    f.close()
+    f = open(path+filename24, "r")
+    y21 = int(f.read())
+    f.close()
+
+    a21 = y21-mini21
+    b21 = maxi21-y21
+    c21 = float(a21/x21)
+    d21 = float(b21/(200-x21))
+    e21 = int(maxi21-d21*200)
+
+    for i in range(0,201):
+        if i <=x21:
+            range12.append(int(c21*i+mini21))
+        else:
+            range12.append(int(d21*i+e21))
+
+    for i in range(0,201):
+        x.append(i)
+
+    #fig = plt.figure()  #新規のグラフを描画
+    plt.xlim([0,200])
+    plt.ylim([1000,2000])
+    plt.plot(x, range12)
+    
+    plt.savefig(path+graphfile1)
+    plt.close("all")
+
+def main():
+    makeGraph()
+    aaa=path+graphfile1
+    bbb=path+graphfile2
+    ccc = "sudo cp "+aaa+" "+bbb
+    #proc.call("sudo cp "+path+graphfile1 path+graphfile2, shell=True)
+    proc.call(ccc, shell=True)
+
+
+if __name__ =='__main__':
+    main()
--- pyfile.py
+++ pyfile.py
@@ -24,9 +24,9 @@ def setup():
 filename41 = "brake_free.txt"
 filename42 = "brake_brake.txt"
 
-
+Graph="Graph.py"
 #*******neutral.html***STEERING********************
-path = "/home/pi/Desktop/TeamA/"  #適当なパスを指定
+path = "/home/pi/Desktop/TeamB/"  #適当なパスを指定
 filename11 = "mini.txt" #最小値
 filename12 = "neut.txt" #中立値
 filename13 = "maxi.txt" #最大値
@@ -176,7 +176,9 @@ def Send23(val):
 #******ページに表示するadjustment_throttle.html******************
 @webiopi.macro
 def GetGraph():
-    proc.call("sudo killall sudo")
+    proc.call("sudo killall sudo", shell=True)
+    proc.call("sudo python3 "+path+Graph+" &", shell=True)
+    #  proc.call("sudo python3 /home/pi/Desktop/TeamB/Graph.py &", shell=True)
 
 @webiopi.macro
 def Load31(val):
